beeae4a5605ad341670b3e82c28df8b8f1d47370
XiaoOOMing <13261675306@163.com>
2018-09-04 17:29:28 +0800

    2数组的交集、并集、差集、对称差集

--- Array/4.23.php
+++ Array/4.23.php
@@ -5,3 +5,26 @@
  * Date: 2018/9/4
  * Time: 17:24
  */
+// 计算并集
+function union($a, $b)
+{
+    return array_merge($a, $b);
+}
+
+// 计算交集
+function intersection($a, $b)
+{
+    return array_intersect($a, $b);
+}
+
+// 差集
+function diff($a, $b)
+{
+    return array_diff($a, $b);
+}
+
+// 对称差
+function merge_diff($a, $b)
+{
+    return array_merge(array_diff($a, $b), array_diff($b, $a));
+}
