f9fb8da379555f694ccdef0f7d96b3aaecd98ea8
XiaoOOMing <13261675306@163.com>
2018-09-07 17:03:29 +0800

    检测表单必填项

--- Form/hello-post.php
+++ Form/hello-post.php
@@ -1,2 +1,7 @@
 <?php
-echo 'Hello ' . $_POST['first_name'];
+// 检测是否传递了 $_POST['first_name']
+if (!filter_has_var(INPUT_POST, 'first_name')) {
+    echo 'You must enter your first name';
+} else {
+    echo 'Hello ' . $_POST['first_name'];
+}
