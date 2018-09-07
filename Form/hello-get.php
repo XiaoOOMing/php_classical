f9a43f6651b803930c44be34152c9a7531c42bdb
XiaoOOMing <13261675306@163.com>
2018-09-07 17:10:41 +0800

    验证POSt数据必须为数字

--- Form/hello-get.php
+++ Form/hello-get.php
@@ -1,5 +1,9 @@
 <form action="<?php echo htmlentities($_SERVER['SCRIPT_NAME'])?>" method="post">
     <p>What's your first name?</p>
     <input type="text" name="first_name">
+    <br>
+    <p>What's your age?</p>
+    <input type="text" name="age">
+    <br>
     <input type="submit" value="Say hello">
 </form>
--- Form/hello-post.php
+++ Form/hello-post.php
@@ -1,7 +1,17 @@
 <?php
 // 检测是否传递了 $_POST['first_name']
-if (!filter_has_var(INPUT_POST, 'first_name')) {
-    echo 'You must enter your first name';
+if (!filter_has_var(INPUT_POST, 'first_name') || $_POST['first_name'] == '') {
+    echo 'You must enter your first name!';
 } else {
-    echo 'Hello ' . $_POST['first_name'];
+    // 检测是否传递了 $_POST['age']
+    if (!filter_has_var(INPUT_POST, 'age') || $_POST['age'] == '') {
+        echo 'You must enter your age!';
+    } else {
+        // 检测 $_POST['age']必须为数字
+        if (!filter_input(INPUT_POST, 'age', FILTER_VALIDATE_INT)) {
+            echo 'Submitted age is invalid!';
+        } else {
+            echo 'Hello ' . $_POST['first_name'] . ', your age is ' . $_POST['age'] . '.';
+        }
+    }
 }
