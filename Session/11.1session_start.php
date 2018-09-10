ff5bdd1f555f1fc4f74f28f6d5d627de74052add
XiaoOOMing <13261675306@163.com>
2018-09-10 17:03:40 +0800

    定时修改session id

--- Session/11.1session_start.php
+++ Session/11.1session_start.php
@@ -6,4 +6,10 @@ if (!isset($_SESSION['visits'])) {
     $_SESSION['visits'] = 0;
 }
 $_SESSION['visits']++;
+// 定时修改会话
+if (!isset($_SESSION['generated']) || $_SESSION['generated'] < (time() - 5)) {
+    session_regenerate_id();
+    $_SESSION['generated'] = time();
+}
 echo "You have visited here {$_SESSION['visits']} times.";
+echo $_SESSION['generated'];
